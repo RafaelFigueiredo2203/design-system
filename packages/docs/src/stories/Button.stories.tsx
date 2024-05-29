28a33c6bfd22aacfc57ae3add2242534cb259f59
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-05-28 22:21:54 -0300

    Documentação de tokens

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps } from '@ignite-ui/react/src/index'
 import { Meta, StoryObj } from '@storybook/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Enviar',
