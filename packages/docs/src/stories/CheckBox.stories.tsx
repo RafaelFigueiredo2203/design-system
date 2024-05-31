6c357c138026a0385eda3f7b205631717af033cf
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-05-31 20:49:24 -0300

    Componente: CheckBox

---
+++ packages/docs/src/stories/CheckBox.stories.tsx
@@ -0,0 +1,24 @@
+import { Box, Text } from '@ignite-ui/react'
+import { CheckBox, CheckBoxProps } from '@ignite-ui/react/src'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/CheckBox',
+  component: CheckBox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'fex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Except term of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckBoxProps>
+
+export const Primary: StoryObj<CheckBoxProps> = {}
